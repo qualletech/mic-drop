b2d68722901c3524056deb4ff1ac01f5efa753f5
vik-wed <wedel.viktoria@gmail.com>
2024-04-26 15:51:04 -0400

    Add meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,14 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     <html lang="en">
       <head>
         <title>Mic Drop</title>
+        <meta name="description" content="Open Mic Dashboard for NYC Comedians" />
+        <meta
+          name="keywords"
+          content="nyc comedians, open mics nyc, open mic, open mics, nyc, nyc comedian, comedy open mic"
+        />
+        <meta name="robots" content="index, follow" />
+        <meta name="author" content="Qualle Tech" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       </head>
       <body>{children}</body>
     </html>
