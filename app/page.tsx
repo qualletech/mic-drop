907f732cc0a6fe16ab85991a10ca32fce877dfa2
vik-wed <wedel.viktoria@gmail.com>
2024-05-03 17:33:22 -0400

    Update page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,15 @@
-"use client"
+import Card from "@/components/Card"
+import prismaClient from "@/prisma/prismaClient"
 
-export default function Page() {
-  return <h1>Open Mic Dashboard for NYC</h1>
+export default async function Page() {
+  const mics = await prismaClient.mic.findMany({ include: { mic_times: true, venue: true } })
+
+  return (
+    <>
+      <h1>Open Mic Dashboard for NYC</h1>
+      {mics.map((m) => (
+        <Card mic={m} />
+      ))}
+    </>
+  )
 }
