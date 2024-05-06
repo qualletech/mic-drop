6e85a40549355cb84d8f6a89f29de073b3151f68
vik-wed <wedel.viktoria@gmail.com>
2024-05-06 17:21:30 -0400

    Fix dashboard styling

--- app/page.tsx
+++ app/page.tsx
@@ -5,11 +5,13 @@ export default async function Page() {
   const mics = await prismaClient.mic.findMany({ include: { mic_times: true, venue: true } })
 
   return (
-    <>
-      <h1>Open Mic Dashboard for NYC</h1>
-      {mics.map((m) => (
-        <Card mic={m} />
-      ))}
-    </>
+    <main className="grid grid-rows-[auto_1fr] gap-3 max-h-full">
+      <h1 className="text-4xl font-extrabold">NYC Open Mics</h1>
+      <div className="grid grid-cols-4 gap-3 overflow-auto max-h-full">
+        {mics.map((m) => (
+          <Card mic={m} />
+        ))}
+      </div>
+    </main>
   )
 }
