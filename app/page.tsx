2e42237a5e0c9d7d7d3244674627d48e4d1344cd
vik-wed <wedel.viktoria@gmail.com>
2024-05-07 16:23:30 -0400

    Implement sorting

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,36 @@ import Card from "@/components/Card"
 import prismaClient from "@/prisma/prismaClient"
 
 export default async function Page() {
-  const mics = await prismaClient.mic.findMany({ include: { mic_times: true, venue: true } })
+  const mics = await prismaClient.mic.findMany({
+    include: { mic_times: true, venue: true },
+  })
+  const weekdayOrder = {
+    monday: 0,
+    tuesday: 1,
+    wednesday: 2,
+    thursday: 3,
+    friday: 4,
+    saturday: 5,
+    sunday: 6,
+  }
+
+  mics.sort((a, b) => {
+    const earliestA = a.mic_times.reduce((earliest, current) => {
+      if (!earliest || weekdayOrder[current.weekday] < weekdayOrder[earliest.weekday]) {
+        return current
+      }
+      return earliest
+    }, null)
+
+    const earliestB = b.mic_times.reduce((earliest, current) => {
+      if (!earliest || weekdayOrder[current.weekday] < weekdayOrder[earliest.weekday]) {
+        return current
+      }
+      return earliest
+    }, null)
+
+    return weekdayOrder[earliestA.weekday] - weekdayOrder[earliestB.weekday]
+  })
 
   return (
     <main className="grid grid-rows-[auto_1fr] gap-3 max-h-full">
