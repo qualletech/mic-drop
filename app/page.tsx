c2a2de0a0b3d34186ac6f69b09a7fc652d57c080
vik-wed <wedel.viktoria@gmail.com>
2024-05-10 15:48:33 -0400

    Fix responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -35,8 +35,8 @@ export default async function Page() {
 
   return (
     <main className="grid grid-rows-[auto_1fr] gap-3 max-h-full overflow-auto">
-      <h1 className="text-4xl font-extrabold">NYC Open Mics</h1>
-      <div className="grid grid-cols-4 gap-3 overflow-auto max-h-full px-12 py-6">
+      <h1 className="text-4xl font-extrabold px-12 pt-6">NYC Open Mics</h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-3 overflow-auto max-h-full px-12 py-6">
         {mics.map((m) => (
           <Card mic={m} />
         ))}
