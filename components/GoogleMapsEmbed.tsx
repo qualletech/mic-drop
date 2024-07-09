4c69b68b785518e4624a458bc66b3f130445bc62
vik-wed <wedel.viktoria@gmail.com>
2024-07-09 15:29:59 -0400

    Update google map embed and remove instagram embed

---
+++ components/GoogleMapsEmbed.tsx
@@ -0,0 +1,12 @@
+export default function GoogleMapsEmbed({ address }: { address: string }) {
+  return (
+    <iframe
+      title="maps"
+      src={`https://maps.google.com/maps?q=${address}&output=embed`}
+      height="400"
+      width="100%"
+      loading="lazy"
+      referrerPolicy="no-referrer-when-downgrade"
+    />
+  )
+}
