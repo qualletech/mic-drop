91a52d14a88aca12209e37145529f162fcd3d423
vik-wed <wedel.viktoria@gmail.com>
2024-08-15 17:39:09 -0400

    Update form styling

--- components/NewMicForm.tsx
+++ components/NewMicForm.tsx
@@ -31,51 +31,57 @@ export default function NewMicForm() {
           className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
         />
       </label>
-      <label htmlFor="sign-up" className="block mb-2 text-sm font-medium">
-        Sign Up Process
-        <input
-          type="text"
-          placeholder="Bucket"
-          id="sign-up"
-          className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
-        />
-      </label>
-      <label htmlFor="ig-handle" className="block mb-2 text-sm font-medium">
-        Instagram Handle
-        <input
-          type="text"
-          placeholder="@glorywholecomedy"
-          id="ig-handle"
-          className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
-        />
-      </label>
-      <label htmlFor="website" className="block mb-2 text-sm font-medium">
-        Website
-        <input
-          type="text"
-          placeholder="https://dixonplace.org/"
-          id="website"
-          className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
-        />
-      </label>
-      <label htmlFor="set-time" className="block mb-2 text-sm font-medium">
-        Set Time
-        <input
-          type="number"
-          placeholder="3"
-          id="set-time"
-          className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
-        />
-      </label>
-      <label htmlFor="payment" className="block mb-2 text-sm font-medium">
-        Payment/Drink Minimum etc
-        <input
-          type="text"
-          placeholder="1 Item Purchase"
-          id="payment"
-          className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
-        />
-      </label>
+      <div className="grid md:grid-cols-2 md:gap-8">
+        <label htmlFor="sign-up" className="block mb-2 text-sm font-medium">
+          Sign Up Process
+          <input
+            type="text"
+            placeholder="Bucket"
+            id="sign-up"
+            className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
+          />
+        </label>
+        <label htmlFor="set-time" className="block mb-2 text-sm font-medium">
+          Set Time
+          <input
+            type="number"
+            placeholder="3"
+            id="set-time"
+            className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
+          />
+        </label>
+      </div>
+      <div className="grid md:grid-cols-2 md:gap-8">
+        <label htmlFor="ig-handle" className="block mb-2 text-sm font-medium">
+          Instagram Handle
+          <input
+            type="text"
+            placeholder="@glorywholecomedy"
+            id="ig-handle"
+            className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
+          />
+        </label>
+        <label htmlFor="website" className="block mb-2 text-sm font-medium">
+          Website
+          <input
+            type="text"
+            placeholder="https://dixonplace.org/"
+            id="website"
+            className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
+          />
+        </label>
+      </div>
+      <div className="grid md:grid-cols-2 md:gap-8">
+        <label htmlFor="payment" className="block mb-2 text-sm font-medium">
+          Payment/Drink Minimum etc
+          <input
+            type="text"
+            placeholder="1 Item Purchase"
+            id="payment"
+            className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
+          />
+        </label>
+      </div>
     </>
   )
 }
