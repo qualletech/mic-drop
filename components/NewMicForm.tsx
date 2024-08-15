f36127dbe04757133d5619f29b46f2849b7630d1
vik-wed <wedel.viktoria@gmail.com>
2024-08-15 17:30:21 -0400

    Update new mic form

--- components/NewMicForm.tsx
+++ components/NewMicForm.tsx
@@ -1,33 +1,80 @@
 export default function NewMicForm() {
   return (
     <>
-      <label>
-        Mic Name
-        <input type="text" />
+      <label htmlFor="mic-name" className="block mb-2 text-sm font-medium">
+        Mic Name <span className="text-red font-bold">*</span>
+        <input
+          required
+          type="text"
+          placeholder="Funny Mic 123"
+          id="mic-name"
+          className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
+        />
       </label>
-      <label>
+      <label htmlFor="venue" className="block mb-2 text-sm font-medium">
+        Venue Name & Borough <span className="text-red font-bold">*</span>
+        <input
+          required
+          type="text"
+          placeholder="Dixon Place, Manhattan"
+          id="venue"
+          className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
+        />
+      </label>
+      <label htmlFor="mic-time" className="block mb-2 text-sm font-medium">
+        Mic Times: Weekday, Time and Reccurence <span className="text-red font-bold">*</span>
+        <input
+          required
+          type="text"
+          placeholder="Every Wednesday 8pm"
+          id="mic-time"
+          className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
+        />
+      </label>
+      <label htmlFor="sign-up" className="block mb-2 text-sm font-medium">
         Sign Up Process
-        <input type="text" />
+        <input
+          type="text"
+          placeholder="Bucket"
+          id="sign-up"
+          className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
+        />
       </label>
-      <label>
-        Instagram Handle <input type="text" />
+      <label htmlFor="ig-handle" className="block mb-2 text-sm font-medium">
+        Instagram Handle
+        <input
+          type="text"
+          placeholder="@glorywholecomedy"
+          id="ig-handle"
+          className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
+        />
       </label>
-      <label>
-        Website <input type="text" />
+      <label htmlFor="website" className="block mb-2 text-sm font-medium">
+        Website
+        <input
+          type="text"
+          placeholder="https://dixonplace.org/"
+          id="website"
+          className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
+        />
       </label>
-      <label>
-        Set Time <input type="number" />
+      <label htmlFor="set-time" className="block mb-2 text-sm font-medium">
+        Set Time
+        <input
+          type="number"
+          placeholder="3"
+          id="set-time"
+          className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
+        />
       </label>
-      <label>
-        Payment/Drink Minimum etc <input type="text" />
-      </label>
-      <label>
-        Venue
-        <input type="text" />
-      </label>
-      <label>
-        Mic Times
-        <input type="text" />
+      <label htmlFor="payment" className="block mb-2 text-sm font-medium">
+        Payment/Drink Minimum etc
+        <input
+          type="text"
+          placeholder="1 Item Purchase"
+          id="payment"
+          className="border border-black/30 text-sm rounded-lg focus:ring-2 focus:  ring-orange focus:outline-none block w-full p-2.5"
+        />
       </label>
     </>
   )
