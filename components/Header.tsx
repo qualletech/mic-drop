be1ade91a8a32a708c6d0a4fd1205e37b35488d9
vik-wed <wedel.viktoria@gmail.com>
2024-09-04 15:50:30 -0400

    Update styling and copy

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,10 +16,10 @@ export default function Header() {
         </button>
         <button
           type="button"
-          className="rounded-lg border-2 border-red bg-light px-3.5 py-2.5 text-md font-bold text-red shadow-sm hover:bg-light/40 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange"
+          className="rounded-lg border-2 border-red px-3.5 py-2.5 text-md font-bold text-red shadow-sm hover:bg-light/40 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange"
           onClick={() => router.push("/contact")}
         >
-          ?
+          Suggestion Box
         </button>
       </div>
     </nav>
