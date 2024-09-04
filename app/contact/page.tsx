9857afc0951d679cb6089a9a9e057992c04a4900
vik-wed <wedel.viktoria@gmail.com>
2024-09-04 16:21:52 -0400

    Update styling

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -42,9 +42,9 @@ export default function Page() {
   }
 
   return (
-    <div className="grid md:grid-cols-[1fr_auto] max-h-full overflow-auto px-12 py-6">
+    <div className="grid md:grid-cols-[1fr_auto] max-h-full overflow-auto px-6 py-3 md:px-12 md:py-6">
       <div className="grid content-start gap-5">
-        <h1 className="text-4xl font-extrabold">Suggestion Box</h1>
+        <h1 className="text-2xl md:text-4xl font-extrabold">Suggestion Box</h1>
         <form className="grid gap-2">
           <label htmlFor="request-type" className="block mb-2 text-sm font-medium">
             Request Type
