b28bee6dc0e17474e87b3b296f1c190bfac8e82f
vik-wed <wedel.viktoria@gmail.com>
2024-09-10 12:04:54 -0400

    Add csp

--- next.config.mjs
+++ next.config.mjs
@@ -15,7 +15,22 @@ const nextConfig = {
           },
           {
             key: "Referrer-Policy",
-            value: "origin-when-cross-origin",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Content-Security-Policy",
+            value: `
+              default-src 'self';
+              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com;
+              style-src 'self' 'unsafe-inline';
+              frame-src https://maps.google.com https://www.google.com;
+              font-src 'self';
+              connect-src 'self' ${process.env.DATABASE_URL};
+              img-src 'self' data: https://maps.googleapis.com https://maps.gstatic.com;
+              form-action 'self';
+              base-uri 'self';
+              object-src 'none';
+            `,
           },
         ],
       },
