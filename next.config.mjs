4d0cd465ab0fbc46b35d5efd811f9a1adf63c21e
vik-wed <wedel.viktoria@gmail.com>
2024-09-10 12:23:44 -0400

    Change csp with google values

--- next.config.mjs
+++ next.config.mjs
@@ -21,13 +21,15 @@ const nextConfig = {
             key: "Content-Security-Policy",
             value: `
               default-src 'self';
-              script-src 'self' 'unsafe-inline' 'unsafe-eval' https://maps.googleapis.com;
-              img-src 'self' data: blob:;
+              script-src 'self' 'unsafe-inline' 'unsafe-eval';
+              img-src 'self' data: blob: https://*.googleapis.com https://*.gstatic.com *.google.com *.googleusercontent.com;
               style-src 'self' 'unsafe-inline';
-              font-src 'self';
+              connect-src 'self' https://*.googleapis.com *.google.com https://*.gstatic.com data: blob:;
+              font-src 'self' https://fonts.gstatic.com;
               object-src 'none';
               form-action 'self';
               base-uri 'self';
+              frame-src *.google.com;
             `.replace(/\n/g, ""),
           },
         ],
